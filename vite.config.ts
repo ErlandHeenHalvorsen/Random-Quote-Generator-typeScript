78ba08a6fdb2e2c9d56689e75450d3afe148c535
GreenGoblin01002 <erlhal01002@stud.noroff.no>
2024-09-20 14:13:23 +0200

    initial commit

---
+++ vite.config.ts
@@ -0,0 +1,7 @@
+import { defineConfig } from "vite";
+export default defineConfig({
+  base: "./", // Ensure relative paths for assets in production
+  build: {
+    outDir: "dist", // Output directory for the build
+  },
+});
